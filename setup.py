1bc35459857548af156612de16ddcb94960ece61
Yuri Shkuro <ys@uber.com>
2016-01-22 17:51:52 -0500

    Back to development: 0.3.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='opentracing_instrumentation',
-    version='0.3.7',
+    version='0.3.8.dev0',
     author='Yuri Shkuro',
     author_email='ys@uber.com',
     description='Tracing Instrumentation using OpenTracing API (http://opentracing.io)',
