90c959262edcae04b8b8a09fe6b45540827528b2
Yuri Shkuro <ys@uber.com>
2018-04-16 22:57:32 -0400

    Restrict tornado<5
    Signed-off-by: Yuri Shkuro <ys@uber.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         'future',
         'wrapt',
-        'tornado>=4.1',
+        'tornado>=4.1,<5',
         'contextlib2',
         'opentracing>=1.1,<2',
         'six',
