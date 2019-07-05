771b7ac68d2b52f7856a4a25a49ae3414b2f8d14
Yuri Shkuro <ys@uber.com>
2019-07-05 01:30:01 -0400

    Back to development: 3.1.1

--- RELEASE.md
+++ RELEASE.md
@@ -3,7 +3,7 @@
 Before new release, add a summary of changes since last version to CHANGELOG.rst
 
 ```
-pip install zest.releaser[recommended]
+pip install 'zest.releaser[recommended]'
 prerelease
 release
 git push origin master --follow-tags
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='opentracing_instrumentation',
-    version='3.1.0',
+    version='3.1.1.dev0',
     author='Yuri Shkuro',
     author_email='ys@uber.com',
     description='Tracing Instrumentation using OpenTracing API '
